3db4ada83cbbefdf4f983d84966b39181f95aa7e
Noel Pinto (QubeHealth) <tmt.6@qubehealth.com>
2024-02-20 11:08:09 +0530

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,48 @@
     <title>Adhoc Tasks Wiki</title>
   </header>
   <div>
-    
+    <h1>Welcome to Task Logger</h1>
+    <div>
+      <input type="text" v-model="newTask" placeholder="Enter a new task">
+      <button @click="addTask">Add Task</button>
+    </div>
+    <div v-if="tasks.length > 0">
+      <h2>Tasks:</h2>
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          {{ task }}
+          <button @click="removeTask(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>No tasks yet. Start adding tasks!</p>
+    </div>
   </div>
 </template>
 
-<style lang="scss"> 
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask.trim());
+        this.newTask = '';
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
 
 </style>
